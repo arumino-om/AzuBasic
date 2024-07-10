fa0a6ea5e51cb4a96b7b3c030b4f3a486f700aaf
Arumino <git@arumino-om.net>
2024-07-11 00:28:53 +0900

    バージョン情報とかを表示させてみる

--- AzuBasic/Program.cs
+++ AzuBasic/Program.cs
@@ -2,8 +2,14 @@
 
 class Program
 {
+    public const int VERSION_MAJOR = 0;
+    public const int VERSION_MINOR = 0;
+    public const int VERSION_PATCH = 1;
+    public const string PROGRAM_NAME = "AzuBasic";
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine($"{PROGRAM_NAME} v{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}");
+        
     }
 }
