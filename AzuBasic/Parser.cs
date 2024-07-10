ff32a2af2d8521dc2200ea0c95f141222b2b731c
Arumino <git@arumino-om.net>
2024-07-11 00:28:42 +0900

    Parserの準備

--- AzuBasic/Parser.cs
+++ AzuBasic/Parser.cs
@@ -1,6 +1,22 @@
-﻿namespace AzuBasic;
+﻿using AzuBasic.Types;
+using AzuBasic.Types.Nodes;
+
+namespace AzuBasic;
 
 public class Parser
 {
+    private Lexer lexer;
+    private Token currentToken;
+
+    public Parser(Lexer lexer)
+    {
+        this.lexer = lexer;
+        this.currentToken = lexer.GetNextToken();
+    }
     
+    public ASTNode Parse()
+    {
+        //TODO: 文法規則に従って解析を行い、ASTを構築
+        return null;
+    }
 }
